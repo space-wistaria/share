d92e02da4e7bfc2bc37ddcbd83609838bdfee0ec
space-wistaria <overseat11@gmail.com>
2020-05-16 09:01:16 +0900

    portfolio_cite

---
+++ Desktop/portfolio_cite/jquary.js
@@ -0,0 +1,33 @@
+$(function(){
+  $(window).scroll(function (){
+      $('.fadein').each(function(){
+          var targetElement = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > targetElement - windowHeight + 200){
+              $(this).css('opacity','1');
+              $(this).css('transform','translateY(0)');
+          }
+      });
+  });
+});
+
+//modal
+$(function(){
+  var winScrollTop;
+  $('.js-modal-open').each(function(){
+      $(this).on('click',function(){
+          //スクロール位置を取得
+          winScrollTop = $(window).scrollTop();
+          var target = $(this).data('target');
+          var modal = document.getElementById(target);
+          $(modal).fadeIn();
+          return false;
+      });
+  });
+  $('.js-modal-close').on('click',function(){
+      $('.js-modal').fadeOut();
+      $('body,html').stop().animate({scrollTop:winScrollTop}, 100);
+      return false;
+  }); 
+});
